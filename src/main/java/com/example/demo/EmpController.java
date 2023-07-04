8812e38d1e42d4973d5f7f598155a07eaa0d9d74
eadabala <eadabala@gmail.com>
2023-07-04 15:35:36 +0530

    added get mapping

--- src/main/java/com/example/demo/EmpController.java
+++ src/main/java/com/example/demo/EmpController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,5 +12,10 @@ public class EmpController {
 	@PostMapping
 	public Emp createEmp(@RequestBody Emp emp) {
 		return emp;
+	}
+	
+	@GetMapping
+	public Emp getEmp() {
+		return new Emp(100,"srinivas100",100);  
 	}
 }
